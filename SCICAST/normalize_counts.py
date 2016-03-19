b53fcbd3b578653668e8f94e3a97c49e0ccf6ee8
idbedead <driver.ian@gmail.com>
2016-03-19 15:03:22 -0700

    python wrapper for edgeR and DESeq2 normalization

--- SCICAST/normalize_counts.py
+++ SCICAST/normalize_counts.py
@@ -1,22 +1,28 @@
 from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage as STAP
 import rpy2.robjects as robjects
+from rpy2 import rinterface as ri
 import os
 
 
-with open('/Users/idriver/RockLab-files/SCICAST/normalize_functions.R', 'r') as f:
-    r_file = f.read()
-norm_functions = STAP(r_file, "normalize_functions")
+norm_func_path = '/Users/idriver/RockLab-files/SCICAST/SCICAST/normalize_functions.R'
+r_source = robjects.r['source']
+r_source(norm_func_path)
+r_make_cpm = robjects.globalenv['make_cpm']
+r_run_deseq2 = robjects.globalenv['run_deseq2']
 
 #path to fpkm file (usually cuffnorm output)
 path_to_file = '/Volumes/Seq_data/count-picard_Pdgfra_ctrl'
 #default file name will use genes.fpkm_table from cuffnorm
 file_name = 'Pdgfra_ctrl_all_count_table.txt'
 #provide base name for output files
-base_name ='Pdgfra_ctrl_counts'
-file_path = path_to_file+'/'+file_name
-group1_terms = 'ctrl1'
-group1_name = 'all'
-group2_terms = 'low'
-group2_name = 'low'
-norm_functions.make_cpm(robjects.r(file_path) , robjects.r(base_name))
-norm_functions.run_deseq2(robjects.r(file_path), robjects.r(base_name), robjects.r(group1_terms), robjects.r(group1_name), robjects.r(group2_terms), robjects.r(group2_name))
+base_name = robjects.r('"Pdgfra_ctrl_counts"')
+file_path = robjects.r('"/Volumes/Seq_data/count-picard_Pdgfra_ctrl/Pdgfra_ctrl_all_count_table.txt"')
+robjects.globalenv['file_path'] = file_path
+robjects.globalenv['base_name'] = base_name
+print(file_path)
+group1_terms = ['ctrl1']
+group1_name = ['all']
+group2_terms = ['low']
+group2_name = ['low']
+r_make_cpm(file_path , base_name)
+dds = r_run_deseq2(file_path, base_name, robjects.vectors.StrVector(group1_terms), robjects.vectors.StrVector(group1_name), robjects.vectors.StrVector(group2_terms), robjects.vectors.StrVector(group2_name))
