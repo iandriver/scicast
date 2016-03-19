6f9b9717a04ef1ba8515fb40877eb8faf94f8c8a
idbedead <driver.ian@gmail.com>
2016-03-19 15:03:54 -0700

    R functions for RNA-seq normaliation

--- SCICAST/normalize_functions.R
+++ SCICAST/normalize_functions.R
@@ -25,13 +25,13 @@ plotDispEsts(dds)
 return(dds)}
 
 make_cpm <- function(path_to_file,file_base_name){
-  library(edgeR)
-	raw.data <- read.csv(file = file_path, header=TRUE, sep="\t", row.names=1)
-	counts <- raw.data[ , -c(1,ncol(raw.data)) ]
-	rownames( counts ) <- row.names(raw.data)
-	cds <- DGEList( counts , group = colnames(counts) )
-	cds <- cds[rowSums(1e+06 * cds$counts/expandAsMatrix(cds$samples$lib.size, dim(cds)) > 1) >= 3, ]
-	cds <- calcNormFactors(cds, method="TMM")
-	cps <- cpm(cds, normalized.lib.sizes=TRUE)
-	write.table(cps, file=paste(file_base_name,"normalized_edgR_cpm.txt",sep='_'), sep="\t", col.names=NA)
+library(edgeR)
+raw.data <- read.csv(file = file_path, header=TRUE, sep="\t", row.names=1)
+counts <- raw.data[ , -c(1,ncol(raw.data)) ]
+rownames( counts ) <- row.names(raw.data)
+cds <- DGEList( counts , group = colnames(counts) )
+cds <- cds[rowSums(1e+06 * cds$counts/expandAsMatrix(cds$samples$lib.size, dim(cds)) > 1) >= 3, ]
+cds <- calcNormFactors(cds, method="TMM")
+cps <- cpm(cds, normalized.lib.sizes=TRUE)
+write.table(cps, file=paste(file_base_name,"normalized_edgR_cpm.txt",sep='_'), sep="\t", col.names=NA)
 }
