2430398226bfef5fc95ba768e23ecd666d791ff9
iandriver <driver.ian@gmail.com>
2016-10-27 16:47:53 -0700

    typo in load modules

--- scicast/__init__.py
+++ scicast/__init__.py
@@ -1,8 +1,8 @@
 from .cluster import *
 from .scicast_argparse import *
 from .sci_load import *
-from .tikinter_scicast import *
+from .tkinter_scicast import *
 
 set()
-__all__ = ["scicast_argparse", "sci_load", "tikinter_scicast"]
+__all__ = ["scicast_argparse", "sci_load", "tkinter_scicast"]
 __version__ = "0.5.1"
