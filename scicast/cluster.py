1ba2b37ab236e4f13c1cab065f70360f6bc13009
iandriver <driver.ian@gmail.com>
2016-12-09 15:11:03 -0800

    fix logfile bug where -use_TSNE wasn't recorded

--- scicast/cluster.py
+++ scicast/cluster.py
@@ -63,6 +63,8 @@ def make_command_log(args,new_filepath):
         command_str+= ' -kmeans_sig_test'
     if args.already_log2:
         command_str+= ' -already_log2'
+    if args.use_TSNE:
+        command_str+= ' -use_TSNE'
     new_file = new_filepath+'/scicast_command_log.txt'
     with open(new_file, 'a+') as f:
         f.write(now.strftime("%Y-%m-%d %H:%M")+'\n')
